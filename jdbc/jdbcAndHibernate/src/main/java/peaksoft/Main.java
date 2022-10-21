0f827708cb9469ef51aebc1be3cfe66913530662
Zhumgalbek <110242941+Zhumgalbek@users.noreply.github.com>
2022-10-21 13:45:22 +0300

    Update Main.java

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -10,37 +10,6 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-//        UserDaoJdbcImpl userDaoJdbc = new UserDaoJdbcImpl();
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
-//        userDaoJdbc.createUsersTable();
-//        userDaoJdbc.dropUsersTable();
-//        userDaoJdbc.saveUser("argen ","gena",(byte)8 );
-//        userDaoJdbc.findByUserById(3);
-//        userDaoJdbc.addUser(8,"juma0","aisal", (byte) 19);
-
-//        userDaoJdbc.saveUser("syimyk ","mena",(byte)8 );
-//        userDaoJdbc.saveUser("Kanat ","Subanov",(byte)13 );
-//        userDaoJdbc.removeUserById(1);
-//        for (User u: userDaoJdbc.getAllUsers()) {
-//            System.out.println(u);
-//        }
-//        userDaoJdbc.cleanUsersTable();
-//        userDaoHibernate.createUsersTable();
-      // userDaoHibernate.createUsersTable();
-        userDaoHibernate.dropUsersTable();
-//        userDaoHibernate.saveUser("Kanat","Subanov", (byte) 23);
-//        userDaoHibernate.saveUser("Zhnarbek","Allanov", (byte) 12);
-//        userDaoHibernate.saveUser("Zanarbek","Allanov", (byte) 12);
-//        userDaoHibernate.saveUser("hanarbek","Allanov", (byte) 12);
- // userDaoHibernate.dropUsersTable();
-//        System.out.println(userDaoHibernate.getAllUsers());
-      //  userDaoHibernate.cleanUsersTable();
-//        userDaoHibernate.removeUserById(1);
-     //   userDaoHibernate.createUsersTable();
-     //   System.out.println(userDaoHibernate.getAllUsers());
-//userDaoHibernate.cleanUsersTable();
-
-       // userDaoHibernate.removeUserById(9);
 
     }
 }
